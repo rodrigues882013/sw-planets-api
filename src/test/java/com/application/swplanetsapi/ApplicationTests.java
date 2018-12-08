f96ed55dc524dc84a6929a87c3b9515a212bff47
Felipe Rodrigues <felipe.rodrigues@mobicare.com.br>
2018-12-07 23:34:53 -0200

    -- rename old class

--- src/test/java/com/application/swplanetsapi/ApplicationTests.java
+++ src/test/java/com/application/swplanetsapi/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.application.swplanetsapi.swplanetsapi;
+package com.application.swplanetsapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SwPlanetsApiApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
